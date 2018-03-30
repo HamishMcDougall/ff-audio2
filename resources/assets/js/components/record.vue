c52aafa90a8e54c77bf3753eaf4256dc049d2002
Hamish McDougall <hamishmcdougall@gmail.com>
2018-03-30 19:00:07 +1100

    save and playback of audio file

---
+++ resources/assets/js/components/record.vue
@@ -0,0 +1,154 @@
+<template>
+
+      <div class="main">
+
+
+          <button type="button" class="btn btn-secondary" v-on:click="toggleRecording">
+            <span class="" v-show="isRecording"></span>
+              <span class="oi oi-microphone" v-show="!isRecording"></span>
+            <span v-show="!isRecording">Start recording</span>
+            <span v-show="isRecording">Stop recording</span>
+          </button>
+
+
+
+
+        <button type="button" class="btn btn-secondary" v-if="dataUrl.length>0" v-on:click="togglePlay">
+            <span class="oi oi-play-circle"> </span>
+              Play recording
+        </button>
+        <button type="button" class="btn btn-secondary" v-if="dataUrl.length>0" v-on:click="deleteRecording">
+          <span class="oi oi-trash"></span>
+            Delete recording
+        </button>
+        <button type="button" class="btn btn-secondary" v-if="dataUrl.length>0">
+          <span class="oi oi-task"></span>
+            Save recording
+          </button>
+
+        <audio id="audio" preload="auto" v-if="dataUrl.length>0" v-bind:src="dataUrl"></audio>
+
+      </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+
+        },
+        data: function () {
+          return {
+         isRecording: false,
+         audioRecorder: null,
+         recordingData: [],
+         dataUrl: ''
+       };
+    },
+    methods:
+    {
+      toggleRecording: function() {
+           this.isRecording = !this.isRecording;
+        var that = this;
+
+        //check toggle of the button
+      if(this.isRecording){
+          if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+         console.log('getUserMedia supported.');
+         navigator.mediaDevices.getUserMedia (
+            // constraints - only audio needed for this app
+            {
+               audio: true
+            })
+
+            // Success callback
+            .then(function(stream) {
+              that.stream = stream;
+              that.audioRecorder = new MediaRecorder(stream, {
+              });
+              that.audioRecorder.start();
+              console.log('Media recorder started');
+
+            })
+
+            // Error callback
+            .catch(function(err) {
+               console.log('The following getUserMedia error occured: ' + err);
+            }
+         );
+      } else {
+       console.log('getUserMedia not supported on your browser!');
+      }
+
+}
+//stop recording
+else{
+    this.audioRecorder.stop();
+        this.audioRecorder.ondataavailable = function(event) {
+        that.recordingData.push(event.data);
+    }
+    this.audioRecorder.onstop = function(event) {
+        console.log('Media recorder stopped');
+        var blob = new Blob(that.recordingData, { type: 'audio/ogg'});
+        that.dataUrl = window.URL.createObjectURL(blob);
+    }
+  }
+
+        },
+
+
+        togglePlay: function() {
+          console.log('Recording audio play');
+          console.log(this.dataUrl);
+          var audioElement = document.getElementById("audio");
+          audioElement.play();
+
+/*pause stuff
+
+      var onplaying = true;
+      var onpause = false;
+
+
+      // On video playing toggle values
+      audioElement.onplaying = function() {
+          onplaying = true;
+          onpause = false;
+      };
+
+      // On video pause toggle values
+      audioElement.onpause = function() {
+          onplaying = false;
+          onpause = true;
+      };
+
+      // Play video function
+      function playVid() {
+          if (audioElement.paused && !onplaying) {
+              audioElement.play();
+          }
+      }
+
+      // Pause video function
+      function pauseVid() {
+          if (!audioElement.paused && !onpause) {
+              audioElement.pause();
+          }
+      }
+      */
+
+
+
+  },
+
+        submitRecording: function() {
+        },
+        deleteRecording: function() {
+          console.log('Deleted recording');
+            this.isRecording = false;
+            this.recordingData = [];
+            this.dataUrl = '';
+        }
+    },
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+
+        <title>Factfind-Audio</title>
+          <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+
+        <!-- open-iconic-bootstrap (icon set for bootstrap) -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=" crossorigin="anonymous" />
+
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div id="app">
+
+              <record-component></record-component>
+
+            </div>
+
+        </div>
+    </body>
+
+
+     <script src="{{ asset('js/app.js') }}"></script>
+</html>
